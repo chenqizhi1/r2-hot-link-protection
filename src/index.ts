6f26049ce2e49f7aa4f85335e54b1d6b4ec89696
chenqizhi1 <huazi1992@hotmail.com>
2025-11-03 18:40:02 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 // 修改这个列表，更新为自己的 domain list
 const ALLOWED = new Set([
-  'blog.cqzrs.top',
+  'cdn.cqzrs.site',
   'eo-oss.cqzrs.top',
-  'cdn.cqzrs.site'
+  'blog.cqzrs.top'
 ]);
 const CORP    = 'same-site';     // same-origin 也行
 const BUCKET  = 'MEDIA';         // 对应 wrangler 的 r2_buckets 绑定名
